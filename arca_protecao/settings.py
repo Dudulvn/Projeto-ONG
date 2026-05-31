dadab237b7146c63df325652e9ca658a3250760c
Dudulvn <eduardo.lvnogueira@gmail.com>
2026-05-31 20:47:20 -0300

    Update settings.py

--- arca_protecao/settings.py
+++ arca_protecao/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # 👈 AQUI
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
